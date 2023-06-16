ab8cc8458310bd33d7f112f3d4c273fe5fa36ea0
SADI Lina <kl_sadi@esi.dz>
2023-06-16 14:18:44 +0100

    photo

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes import point_d_interet , adresse , theme , categorie , horaires_acces , commentaire , tourist_user , evenement , type_event,moyen_transport
+from routes import point_d_interet , adresse , theme , categorie , photo ,horaires_acces , commentaire , tourist_user , evenement , type_event,moyen_transport
 from config.db import engine
 
 app = FastAPI()
@@ -24,6 +24,7 @@ app.include_router(tourist_user.router)
 app.include_router(evenement.router)
 app.include_router(type_event.router)
 app.include_router(moyen_transport.router)
+app.include_router(photo.router)
 
 # Create the database tables
 from models.point_d_interet import Base
@@ -36,5 +37,7 @@ from models.tourist_user import Base
 from models.type_event import Base
 from models.evenement import Base
 from models.moyen_transport import Base
+from models.photo import Base
+
 
 Base.metadata.create_all(bind=engine)
